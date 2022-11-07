eff7802e90e2b6302a8f3b70e94fadc9f4657cb5
Ethan Kim <117482262+EthanKim4259@users.noreply.github.com>
2022-11-07 11:41:33 +0000

    Please please please please please please work

--- server.js
+++ server.js
@@ -9,18 +9,14 @@ const dbWrapper = new DbWrapper(process.env.URL);
 if (process.env.REPLIT) app.use("/public", express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "survey.html"));
-});
-
 let type = 0;
-app.get("/get_survey", (req, res) => {
+app.get("/api/get_survey", (req, res) => {
 	type ^= 1;
 	res.json({type});
 });
 
 let index = 0;
-app.post("/post_survey", async (req, res) => {
+app.post("/api/post_survey", async (req, res) => {
 	const {type, hours, classes, difficulty} = req.body;
 	await dbWrapper.addData(type, hours, classes, difficulty);
 	res.sendFile(path.join(__dirname, "finish.html"));
